9268396dae8f3819f0ffa2ec4d5649df727775fb
euge <eugeni.puigdomenech@gmail.com>
2026-09-22 13:56:30 +0200

    classe.22.09-commit2

--- Punt2d/Rectangle2D.java
+++ Punt2d/Rectangle2D.java
@@ -2,4 +2,15 @@ package Punt2d;
 
 public class Rectangle2D {
     
+    //area = costata*costatb
+    public area(){
+        
+    }
+
+    //perimetre = costata*2+costatb*2
+    public perimetre(){
+
+    }
+
+
 }
